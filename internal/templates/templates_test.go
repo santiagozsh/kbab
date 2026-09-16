ae892c8fcd8804c579db516f393e0a95b7cbe271
santiago <sanrodsuz@pm.me>
2026-09-16 15:39:37 -0500

    feat(templates): implement Python templates and add syntax validation tests

--- internal/templates/templates_test.go
+++ internal/templates/templates_test.go
@@ -3,6 +3,7 @@ package templates_test
 import (
 	"io/fs"
 	"testing"
+	"text/template"
 
 	"github.com/santiagozsh/kbab/internal/templates"
 )
@@ -22,6 +23,26 @@ func TestFSContainsAllTemplates(t *testing.T) {
 			_, err := fs.Stat(templates.FS, file)
 			if err != nil {
 				t.Fatalf("expected %s to be embedded in FS, got error: %v", file, err)
+			}
+		})
+	}
+}
+
+func TestFS_AllTemplatesAreValidSyntax(t *testing.T) {
+	templatesToParse := []string{
+		"go/Dockerfile.tmpl",
+		"go/dockerignore.tmpl",
+		"node/Dockerfile.tmpl",
+		"node/dockerignore.tmpl",
+		"py/Dockerfile.tmpl",
+		"py/dockerignore.tmpl",
+	}
+
+	for _, tmplPath := range templatesToParse {
+		t.Run(tmplPath, func(t *testing.T) {
+			_, err := template.ParseFS(templates.FS, tmplPath)
+			if err != nil {
+				t.Fatalf("failed to parse template %s: %v", tmplPath, err)
 			}
 		})
 	}
